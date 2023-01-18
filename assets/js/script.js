0013f01acd9d7e3ac63e6976465d4cdfda89059b
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-01-18 08:12:34 +0000

    refactor the timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
+// Get elements
 var currentDay = $("#currentDay")
+var timeBlocks = $(".container")
 
-currentDay.text(moment().format("dddd MMMM Do"))
+// Creating elements
+var hourCol = $("<div></div")
+hourCol.attr('class', 'hour col-2')
+
+var textAreaCol = $("<textarea></textarea>")
+textAreaCol.attr('class', 'textarea present col-8')
+
+var saveCol = $("<div></div>")
+saveCol.attr('class', 'saveBtn col-2')
+
+var row9 = $('<div></div>')
+row9.attr('class', 'row')
+row9.append(hourCol)
+row9.append(textAreaCol)
+row9.append(saveCol)
+
+
+// Moment.js
+function displayTime(){
+var now = moment().format('DD MMM YYYY [at] hh:mm:ss a')
+currentDay.text(now)
+}
+setInterval(displayTime, 1000)
+
+// currentDay.text(now.format("dddd MMMM Do"))
+
+// TimeBlocks
+// timeBlocks.appendChild(row).text("It is the Time Blocks")
+row9.appendTo(timeBlocks)
