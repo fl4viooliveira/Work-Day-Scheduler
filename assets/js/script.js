9729b4b046413a44eeb724b5781f2df852198397
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-01-19 08:01:58 +0000

    Add time blocks
    Create a function to set one time row to each work hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,42 @@
 // Get elements
-var currentDay = $("#currentDay")
-var timeBlocks = $(".container")
-
-// Creating elements
-var hourCol = $("<div></div")
-hourCol.attr('class', 'hour col-2')
-
-var textAreaCol = $("<textarea></textarea>")
-textAreaCol.attr('class', 'textarea present col-8')
-
-var saveCol = $("<div></div>")
-saveCol.attr('class', 'saveBtn col-2')
-
-var row9 = $('<div></div>')
-row9.attr('class', 'row')
-row9.append(hourCol)
-row9.append(textAreaCol)
-row9.append(saveCol)
-
+var currentDay = $("#currentDay");
+var timeBlocks = $(".container");
 
 // Moment.js
-function displayTime(){
-var now = moment().format('DD MMM YYYY [at] hh:mm:ss a')
-currentDay.text(now)
+
+function displayTime() {
+  var now = moment().format("DD MMM YYYY [at] hh:mm:ss a");
+  currentDay.text(now);
 }
-setInterval(displayTime, 1000)
 
-// currentDay.text(now.format("dddd MMMM Do"))
+function hourOfDay() {
+  var hour = moment().hour();
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+  return hour;
+}
 
-// TimeBlocks
-// timeBlocks.appendChild(row).text("It is the Time Blocks")
-row9.appendTo(timeBlocks)
+var workHours = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7];
+
+console.log(hourOfDay());
+
+
+function setTimeBlocks() {
+  for (var i = 0; i < workHours.length; i++) {
+    var timeRow = $(`<div id="id-${i}" class="row">
+      <div class="col-2 hour"></div>
+        <textarea class="col-8 description "></textarea>
+        <button class="col-2 saveBtn">
+          <i class="fas fa-save"></i>
+        </button>
+      </div>`);
+    timeBlocks.append(timeRow);
+
+    // if(workHours[i] < hourOfDay){
+    // }
+  }
+}
+
+setTimeBlocks();
+setInterval(displayTime, 1000);
